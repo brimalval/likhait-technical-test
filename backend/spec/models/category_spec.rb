e67e4af4949a5264bf5570b484b2a63a961e91a2
brimalval <brianmalcolm.v@gmail.com>
2026-07-18 16:50:09 +0800

    feat: validate category names

--- backend/spec/models/category_spec.rb
+++ backend/spec/models/category_spec.rb
@@ -1,4 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+  describe 'validations' do
+    it 'requires a name' do
+      category = Category.new(name: '')
+
+      expect(category).not_to be_valid
+      expect(category.errors[:name]).to include("can't be blank")
+    end
+
+    it 'requires a unique name' do
+      Category.create!(name: 'Food')
+      duplicate = Category.new(name: 'Food')
+
+      expect(duplicate).not_to be_valid
+      expect(duplicate.errors[:name]).to include('has already been taken')
+    end
+  end
 end
